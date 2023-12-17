7e1a6711365ed52f01efbccb1cb9a6bafea71831
User Name <Email@example.com>
2023-12-18 02:49:17 +0300

    ETL Service

--- postgres_to_es/config.py
+++ postgres_to_es/config.py
@@ -1,5 +1,5 @@
 from typing import Optional, Dict
-from pydantic import BaseModel
+from pydantic import BaseModel #for data validation and settings management that's based on Python type hints.
 
 
 class DSNSettings(BaseModel):
@@ -12,26 +12,36 @@ class DSNSettings(BaseModel):
 
 
 class PostgresSettings(BaseModel):
+    """
+        Class for validating connection settings to postgresql
+    """
     dsn: DSNSettings
     limit: Optional[int]
 
 
 class ESHost(BaseModel):
+
     host: str
     port: int
 
 
 class ESSettings(BaseModel):
+    """
+    Class for validating connection settings to Elasticsearch
+    """
     default_host: ESHost
     index_name: str
     index_config: str
 
 
 class ETLSettings(BaseModel):
+    """
+    Class for validating connection settings to ETL-process
+    """
     postgres: PostgresSettings
     es: ESSettings
     fetch_delay: float
-    state_file_name: str  # state_file_name
+    state_file_name: str  # state_file_name: -	The time of the last update of the Elasticsearch index will be saved in it
     log_status_period: float
 
 
